60b3835f4b7f67f33cf78838d8b1a2576cbbe3aa
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-25 16:21:15 +0200

    commit changes

--- 02_code-express_post_body/app.js
+++ 02_code-express_post_body/app.js
@@ -14,6 +14,14 @@
  
  // SET THE STATIC FOLDER FOR PUBLIC FILES
  app.use(express.static(__dirname + "/public"));
+
+ // need to tell the app to use something that is able to read from the body:
+ // asking express to look in the body and give a json translation that can be used in JS
+ // Necessary to read data from the body and will be available in express under body object
+ app.use(express.json())
+ app.use(express.urlencoded({
+   extended: true
+ }))
  
  // REGISTER THE PARTIAL
  hbs.registerPartials(__dirname + "/views/partials");
@@ -23,7 +31,13 @@
  // ROUTES
  app.get("/", (req, res) => {
    console.log(req);
-   res.send("You reached the root of the server");
+   res.render("index");
  });
 
- app.listen(3000)
+ app.post("/search", (req, res) => {
+   const city = req.body.city // city is the name on the form we are referring to
+   res.render('search')
+ }) // telling express to listen for a post verb, only thing we then need to do is send the value in the search hbs to the current template
+
+
+ app.listen(PORT)
